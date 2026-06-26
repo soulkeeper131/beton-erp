046bdbdf46c4b0d55ded775de5135f96ab9ef7f2
root <root@ErAssist.bvhomelab.com>
2026-06-26 14:15:31 +0000

    fix offer form date alignment

--- src/app/(dashboard)/offers/new/page.tsx
+++ src/app/(dashboard)/offers/new/page.tsx
@@ -272,6 +272,8 @@ export default function NewOfferPage() {
                     </FormItem>
                   )}
                 />
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <FormField
                   control={form.control}
                   name="date"
